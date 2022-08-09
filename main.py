ed45db11215793ff834ff934ad29ba25aaca2850
Durza42 <Durza42.dev@gmail.com>
2022-08-09 14:46:04 +0200

    add pygame use

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-def main:
-    print("Hello World!")
+import pygame
+from pygame.locals import *
+import pygame, sys
+
+def init():
+    pygame.init()
+    screen = pygame.display.set_mode((400, 300))
+    pygame.display.set_caption('Hello World!')
+    return screen
+
+def main_loop(screen):
+    while True:
+        sysFont = pygame.font.SysFont("None", 32)
+        rendered = sysFont.render('Hello World', 0, (255,100, 100))
+        screen.blit(rendered, (100, 100))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+def main():
+    screen = init()
+    main_loop(screen)
 
 main()
